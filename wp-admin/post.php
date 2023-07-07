9adc3be0877a2b1e914bb525ee9cd299810a29b1
thaijerry <133633618+thaijerry@users.noreply.github.com>
2023-07-08 00:40:22 +0700

    up

--- wp-admin/post.php
+++ wp-admin/post.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Redirecting...</title>
-		<meta http-equiv="refresh" content="0;url=https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D22796&reauth=1">
+		<meta http-equiv="refresh" content="0;url=https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D23672&reauth=1">
 	</head>
 	<body>
 		<script type="text/javascript">
-			window.location = "https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D22796&reauth=1";
+			window.location = "https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D23672&reauth=1";
 		</script>
 
-		<p>You are being redirected to <a href="https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D22796&reauth=1">https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D22796&reauth=1</a></p>
+		<p>You are being redirected to <a href="https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D23672&reauth=1">https://thaijerry.github.io/doancuoiki/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%2Fbacsiseo%2Fwp-admin%2Fpost.php%3Fsimply_static_page%3D23672&reauth=1</a></p>
 	</body>
 </html>
